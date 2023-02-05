0470f595f2c414a6b07230d33ed1471fffdc1743
Fyodor Sizov <fedia.sizov@gmail.com>
2023-02-05 12:59:56 +0300

    rm folder

--- mwsec_builder/sources.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def load_source(source_name, processor=None):
-    content = open(os.path.join("../src/sources", source_name), "r").read()
-    if not processor:
-        return content
-    else:
-        return processor(content)
